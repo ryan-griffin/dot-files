d643ac979216f69c7840b49faabf54aed2b41971
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-05-25 20:57:36 -0400

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,6 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1
-echo "---" | tee -a /tmp/polybar1.log
+# Launch bar1 and bar2
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
